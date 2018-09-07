9e3adb9d4977a5e0c1aef62cda61268d4ccc663a
Stone <shitong@vite.org>
2018-09-07 10:47:43 +0800

    Add network

---
+++ ViteTests/ViteNetworkTests.swift
@@ -0,0 +1,71 @@
+//
+//  ViteNetworkTests.swift
+//  ViteTests
+//
+//  Created by Stone on 2018/9/7.
+//  Copyright © 2018年 vite labs. All rights reserved.
+//
+
+import XCTest
+import PromiseKit
+@testable import Vite
+
+class ViteNetworkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+    func async(_ block: ( @escaping () -> () ) -> ()) {
+
+        let expect = expectation(description: "method")
+        block {
+            expect.fulfill()
+        }
+        waitForExpectations(timeout: 15, handler: nil)
+        print("🍺🍺🍺🍺🍺🍺")
+
+    }
+
+    func testSnapshotChainProvider() {
+
+        async { (completion) in
+            let provider = SnapshotChainProvider(server: RPCServer.shared)
+            _ = provider.height().done { height in
+                print("🏆snapschot china height: \(height)")
+                completion()
+            }
+        }
+
+    }
+
+    func testAccountProvider() {
+        async { (completion) in
+            let accountProvider = AccountProvider(server: RPCServer.shared)
+            _ = accountProvider.getTransactions(address: Address(string: "vite_7945df07bbf55f5afc76360a263b0870795ce5d1ecea36b786")!).done { transactions in
+                print("🏆\(transactions)")
+                completion()
+            }
+        }
+
+    }
+    
+}
