c85662507ce48c537377df6a06d2a86d76a81a20
water <daima.cn@gmail.com>
2018-09-16 15:17:49 +0800

    fix  export mnemonic view

--- Vite/Business/Account/Mnemonic/ExportMnemonicViewController.swift
+++ Vite/Business/Account/Mnemonic/ExportMnemonicViewController.swift
@@ -11,90 +11,60 @@ import SnapKit
 import Vite_keystore
 
 class ExportMnemonicViewController: BaseViewController {
-    fileprivate var viewModel: BackupMnemonicVM
-
-    init() {
-        self.viewModel = BackupMnemonicVM()
-        super.init(nibName: nil, bundle: nil)
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self._setupView()
-        self._bindViewModel()
     }
 
-    lazy var tipContentTitleLab: UILabel = {
-        let tipContentTitleLab = UILabel()
-        tipContentTitleLab.textAlignment = .left
-        tipContentTitleLab.font =  AppStyle.descWord.font
-        tipContentTitleLab.textColor  = AppStyle.descWord.textColor
-        tipContentTitleLab.text =  R.string.localizable.mnemonicBackupPageTitle.key.localized()
-        return tipContentTitleLab
-    }()
-
-    lazy var tipContentLab: UILabel = {
-        let tipContentLab =  UILabel()
-        tipContentLab.numberOfLines = 0
-        tipContentLab.textColor = .black
-        return tipContentLab
+    lazy var contentTextView: UITextView = {
+        let contentTextView =  UITextView()
+        contentTextView.font = Fonts.Font18
+        contentTextView.backgroundColor = Colors.bgGray
+        contentTextView.textColor = Colors.descGray
+        contentTextView.text = WalletDataService.shareInstance.defaultWalletAccount?.mnemonic
+        contentTextView.layer.masksToBounds = true
+        contentTextView.layer.cornerRadius = 2
+        contentTextView.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        contentTextView.isEditable = false
+        return contentTextView
     }()
 
     lazy var confirmBtn: UIButton = {
-        let confirmBtn = UIButton()
-        confirmBtn.setTitle(R.string.localizable.mnemonicBackupPageTipNextBtnTitle.key.localized(), for: .normal)
-        confirmBtn.setTitleColor(.black, for: .normal)
-        confirmBtn.backgroundColor = .orange
+        let confirmBtn = UIButton.init(style: .blue)
+        confirmBtn.setTitle(R.string.localizable.confirm.key.localized(), for: .normal)
         confirmBtn.addTarget(self, action: #selector(confirmBtnAction), for: .touchUpInside)
         return confirmBtn
     }()
 }
 
 extension ExportMnemonicViewController {
-    private func _bindViewModel() {
-        _ = self.viewModel.mnemonicWordsStr.asObservable().bind(to: self.tipContentLab.rx.text)
-    }
 
     private func _setupView() {
         self.view.backgroundColor = .white
-        self.title = R.string.localizable.mnemonicBackupPageTitle.key.localized()
+        navigationTitleView = NavigationTitleView(title: R.string.localizable.exportPageTitle.key.localized())
 
         self._addViewConstraint()
     }
     private func _addViewConstraint() {
-
-        self.view.addSubview(self.tipContentTitleLab)
-        self.tipContentTitleLab.snp.makeConstraints { (make) -> Void in
-            make.height.equalTo(30)
-            make.left.equalTo(self.view).offset(30)
-            make.top.equalTo(self.view).offset(30)
-        }
-
-        self.view.addSubview(self.tipContentLab)
-        self.tipContentLab.snp.makeConstraints { (make) -> Void in
-            make.width.equalTo(200)
-            make.height.equalTo(200)
-            make.centerX.equalTo(self.view)
-            make.top.equalTo(self.tipContentTitleLab.snp.bottom).offset(10)
+        self.view.addSubview(self.contentTextView)
+        self.contentTextView.snp.makeConstraints { (make) -> Void in
+            make.left.equalTo(self.view).offset(24)
+            make.right.equalTo(self.view).offset(-24)
+            make.height.equalTo(142)
+            make.top.equalTo((self.navigationTitleView?.snp.bottom)!).offset(10)
         }
 
         self.view.addSubview(self.confirmBtn)
         self.confirmBtn.snp.makeConstraints { (make) -> Void in
-            make.width.equalTo(130)
             make.height.equalTo(50)
-            make.centerX.equalTo(self.view)
-            make.bottom.equalTo(self.view).offset(-100)
+            make.left.equalTo(self.view).offset(24)
+            make.right.equalTo(self.view).offset(-24)
+            make.bottom.equalTo(self.view.safeAreaLayoutGuideSnp.bottom).offset(-24)
         }
     }
 
     @objc func confirmBtnAction() {
-        CreateWalletService.sharedInstance.walletAccount.mnemonic = self.viewModel.mnemonicWordsStr.value
-        let vc = AffirmInputMnemonicViewController.init(mnemonicWordsStr: self.viewModel.mnemonicWordsStr.value)
-        self.navigationController?.pushViewController(vc, animated: true)
+        self.navigationController?.popViewController(animated: true)
     }
 }
