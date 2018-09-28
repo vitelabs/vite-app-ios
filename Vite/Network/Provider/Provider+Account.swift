cdf05c23132e161d3bafbf1c8aff709fbb726f0e
Stone <shitong@vite.org>
2018-09-28 17:07:53 +0800

    fix change address

--- Vite/Network/Provider/Provider+Account.swift
+++ Vite/Network/Provider/Provider+Account.swift
@@ -19,16 +19,7 @@ extension Provider {
             Session.send(request) { result in
                 switch result {
                 case .success(let (balanceInfos, unConfirmedInfos)):
-                    var ret = [BalanceInfo]()
-                    for balanceInfo in balanceInfos {
-                        var info = balanceInfo
-                        for unConfirmedInfo in unConfirmedInfos where balanceInfo.token.id == unConfirmedInfo.token.id {
-                            info.fill(unconfirmedBalance: unConfirmedInfo.unconfirmedBalance, unconfirmedCount: unConfirmedInfo.unconfirmedCount)
-                            break
-                        }
-                        ret.append(info)
-                    }
-                    seal.fulfill(ret)
+                    seal.fulfill(BalanceInfo.mergeBalanceInfos(balanceInfos, unConfirmedInfos: unConfirmedInfos))
                 case .failure(let error):
                     seal.reject(error)
                 }
