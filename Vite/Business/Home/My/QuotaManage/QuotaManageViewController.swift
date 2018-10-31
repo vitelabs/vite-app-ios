7a2b3a5081e58faf11a80315aaa82465090dc845
water <daima.cn@gmail.com>
2018-10-31 15:23:05 +0800

    ios10 nav bar right item

--- Vite/Business/Home/My/QuotaManage/QuotaManageViewController.swift
+++ Vite/Business/Home/My/QuotaManage/QuotaManageViewController.swift
@@ -31,14 +31,8 @@ class QuotaManageViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.setupView()
+        setupView()
         initBinds()
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-//        self.navigationController?.setNavigationBarHidden(true, animated: false)
-//        self.navigationController?.setNavigationBarHidden(false, animated: false)
     }
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
